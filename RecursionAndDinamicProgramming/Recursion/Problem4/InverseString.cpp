19688964f47557ad6ee93c5604ad2b8bff496f53
Ronaldo Silva <60142058+ronaldosilva19@users.noreply.github.com>
2023-03-20 12:56:23 -0300

    Update InverseString.cpp

--- RecursionAndDinamicProgramming/Recursion/Problem4/InverseString.cpp
+++ RecursionAndDinamicProgramming/Recursion/Problem4/InverseString.cpp
@@ -1,1 +1,32 @@
+// Create by ronaldo 03/20/2023
+// Problem link description.
+// https://leetcode.com/problems/reverse-string/
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int begin = 0;
+        int end = s.size() - 1;
+        
+       while(begin < end){
+           swap(s[begin], s[end]);
+           begin++;
+           end--;
+       }
+        for(int i = 0; i < s.size(); i++){
+            cout << s[i] << " ";
+        }
+        cout << "\n";
+    }
+};
+
+int main(){
+  Solution solution;
+  vector<char> arr = {'R', 'O', 'N', 'A', 'L', 'D', 'O'};
+  solution.reverseString(arr);
+  return 0;
+}
